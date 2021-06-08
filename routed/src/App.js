24dd33e8b5a7fa81702f52851347b01d86d7b048
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-08 16:33:37 -0400

    Update App.js

--- routed/src/App.js
+++ routed/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const [page, setPage] = useState("home");
+
+  const goToPage = (newPage) => {
+    setPage(newPage);
+  };
+
+  const showPage = () => {
+    if (page === "home") return <Home />;
+    if (page === "eat") return <Eat />;
+    if (page === "drink") return <Drink />;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <nav>
+        <a onClick={() => goToPage("drink")}>Drink</a>
+        <a onClick={() => goToPage("eat")}>Eat</a>
+        <a onClick={() => goToPage("home")}>Home</a>
+      </nav>
+      {showPage()}
+    </main>
   );
 }
 
+
 export default App;
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
